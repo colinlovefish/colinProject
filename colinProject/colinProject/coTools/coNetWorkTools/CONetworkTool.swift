5705346ff71e5000446fbfbc60cf0eac2c963490
yinghu <colinqu@zhen22.com>
2017-06-05 22:23:50 +0800

    NetworkTool

---
+++ colinProject/colinProject/coTools/coNetWorkTools/CONetworkTool.swift
@@ -0,0 +1,24 @@
+//
+//  CONetworkTool.swift
+//  colinProject
+//
+//  Created by yinghu on 2017/6/5.
+//  Copyright © 2017年 yinghu. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CONetworkTool: NSObject {
+
+    static let shareInstance:CONetworkTool = {
+        let tools = CONetworkTool()
+        return tools
+    }()
+}
+
+
+extension CONetworkTool {
+//    post
+
+}
